2956d3eb5a86aef1c6ced0676955193e1d36be7a
Michael Aubry <michealaubrydesign@gmail.com>
2023-09-01 13:17:12 +0200

    Correction filtres fonctionnels + commentaires

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -10,14 +10,26 @@ import "./style.css";
 const PER_PAGE = 9;
 
 const EventList = () => {
-  const { data, error } = useData();
-  const [type, setType] = useState();
-  const [currentPage, setCurrentPage] = useState(1);
+  const { data, error } = useData(); // Récupération des données et erreurs depuis le contexte
+  const [type, setType] = useState(); // État pour le type d'événement sélectionné
+  const [currentPage, setCurrentPage] = useState(1); // État pour la page actuelle
+  // Filtrage des événements en fonction du type et de la pagination
   const filteredEvents = (
     (!type
       ? data?.events
       : data?.events) || []
-  ).filter((event, index) => {
+  ).filter((event) => {
+    if (!type) {
+      return true
+    }
+    if (
+      event.type === type
+    ) {
+      return true;
+    }
+    return false;
+  })
+  .filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
@@ -26,11 +38,17 @@ const EventList = () => {
     }
     return false;
   });
+
+  // Fonction pour changer le type d'événement sélectionné et réinitialiser la page
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
+  // Calcul du nombre total de pages en fonction des événements filtrés
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  // Création d'une liste de types d'événements uniques
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
@@ -39,13 +57,16 @@ const EventList = () => {
         "loading"
       ) : (
         <>
+        {/* Sélection du type d'événement */}
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
+          {/* Affichage des événements filtrés */}
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
+              // Affichage d'un événement dans un modal
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
@@ -59,8 +80,10 @@ const EventList = () => {
               </Modal>
             ))}
           </div>
+          {/* Affichage de la pagination */}
           <div className="Pagination">
             {[...Array(pageNumber || 0)].map((_, n) => (
+              // Lien vers une page spécifique
               // eslint-disable-next-line react/no-array-index-key
               <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
